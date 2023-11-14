c88aead53c5386383e1d5fe7ccf295db05445ec7
Marouane LOUDINI <sam@google.com>
2023-11-14 17:27:10 +0100

    page accueil ok

--- index.php
+++ index.php
@@ -4,13 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ClubPrime</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&family=Open+Sans:wght@300;400;500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
     <div class="accueil">
-        <nav>
-            
-        </nav>
+        <div class="navbar">
+        <img class="logo" src="./ressources/imagespage1/Background.png" alt="Logo">
+            <nav>
+                <a class="acc" href="#">Accueil</a>
+                <a href="#">Podium</a>
+                <a href="#">Classement</a>
+                <a href="#">Équipes</a>
+            </nav>
+            <div class="recherche">
+                <img src="./ressources/imagespage1/recherche.svg" alt="recherche">
+            </div>
+        </div>
+        <div class="corp">
+            <h1>Le Top 10 de l'excellence.</h1>
+            <a href="#">Découvrir le podium</a>
+        </div>
+    </div>
+    <div class="podium">
+        
     </div>
 </body>
 </html>
