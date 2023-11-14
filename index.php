a87396cfa50adc9f22c56ce925670678dadedb35
mrn21263 <marouane8loudini@gmail.com>
2023-11-14 15:52:27 +0100

    Ajout image 2

--- index.php
+++ index.php
@@ -16,3 +16,42 @@
         $imageSrc = 'data:image/jpeg;base64,' . base64_encode($imageData);
     ?>
     <img src="<?php echo $imageSrc; ?>" alt="Photo du produit">
+
+    if (isset($_FILES['photo'])) {
+            $imageData = $_FILES['photo']['tmp_name'];
+
+            // Obtenir le type de l'image
+            $imageType = exif_imagetype($imageData);
+
+            // Vérifier le type d'image et charger l'image correspondante
+            if ($imageType === IMAGETYPE_JPEG) {
+                $sourceImage = imagecreatefromjpeg($imageData);
+            } elseif ($imageType === IMAGETYPE_PNG) {
+                $sourceImage = imagecreatefrompng($imageData);
+            } elseif ($imageType === IMAGETYPE_GIF) {
+                $sourceImage = imagecreatefromgif($imageData);
+            } else {
+                // Type d'image non supporté
+                // Gérez l'erreur ou affichez un message approprié à l'utilisateur
+            }
+
+            $newWidth = 400;
+            $newHeight = 400;
+
+            // Créer une nouvelle image avec les nouvelles dimensions
+            $resizedImage = imagecreatetruecolor($newWidth, $newHeight);
+
+            // Redimensionner l'image d'origine vers la nouvelle image
+            imagecopyresampled($resizedImage, $sourceImage, 0, 0, 0, 0, $newWidth, $newHeight, imagesx($sourceImage), imagesy($sourceImage));
+
+            // Convertir l'image redimensionnée en une chaîne de données
+            ob_start();
+            imagejpeg($resizedImage);
+            $imageDataResized = ob_get_clean();
+
+            $pdo = connectDB();
+            $produitModel = new ProduitModel($pdo);
+            $produitModel->ajouterProduit($nom, $reference, $description, $prix_ht, $imageDataResized, $prix_ttc, $taux_tva, $couleur, $made_in_france, $longueur, $largeur, $hauteur, $poids_net, $disponibilite);
+
+            imagedestroy($sourceImage);
+            imagedestroy($resizedImage);
