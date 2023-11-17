2f870303ee0328f9698eaba3215906f085f3b042
Marouane LOUDINI <sam@google.com>
2023-11-17 17:22:34 +0100

    podium tjr

--- index.php
+++ index.php
@@ -25,33 +25,63 @@
         </div>
         <div class="corp">
             <h1>Le Top 10 de l'excellence.</h1>
-            <a href="#">Découvrir le podium</a>
+            <a href="#podium">Découvrir le podium</a>
         </div>
+
+        <script>
+        var defaultImage = "../ressources/imagespage1/imageequipe/mancity.png";
+        var newImage = "../ressources/imagespage1/imageequipe/bayernmunich.png";
+
+        document.querySelector('.general').addEventListener('mouseover', function() {
+            document.getElementById('podium').style.backgroundImage = "url( + newImage + )";
+        });
+
+        document.querySelector('.general2').addEventListener('mouseout', function() {
+            document.getElementById('podium').style.backgroundImage = "url(" + defaultImage + ")";
+        });
+    </script>
+
     </div>
-    <div class="podium">
+    <div class="podium" id="podium">
         <div class="date">
-        
+        <select id="quantity" name="quantity">
+            <option value="2023/2024">2023/2024</option>
+            <option value="2022/2023">2022/2023</option>
+        </select>
         </div>
         <div class="place">
             <div class="ranked">
-                <div class="deux">
-                    <p>Bayern Munich</p>
-                    <div class="coupe2">
-                        <p>2</p>
+
+                <div class="general">
+                    <img src="./ressources/logoequipe/Bayern_München.png" alt="logo du bayern">
+                    <div class="deux">
+                        <p>Bayern Munich</p>
+                        <div class="coupe2">
+                            <p>2</p>
+                        </div>
                     </div>
                 </div>
-                <div class="un">
-                    <p>Manchester City</p>
-                    <div class="coupe1">
-                        <p>1</p>
+
+                <div class="general2">
+                    <img src="./ressources/logoequipe/Manchester_City.png" alt="logo du mancherster city">
+                    <div class="un">
+                        <p>Manchester City</p>
+                        <div class="coupe1">
+                            <p>1</p>
+                        </div>
                     </div>
                 </div>
-                <div class="trois">
-                    <p>Real Madrid</p>
-                    <div class="coupe3">
-                        <p>3</p>
+
+                <div class="general3">
+                    <img src="./ressources/logoequipe/chelsea.png" alt="logo du real madrid">
+                    <div class="trois">
+                        <p>Chelsea</p>
+                        <div class="coupe3">
+                            <p>3</p>
+                        </div>
                     </div>
                 </div>
+
             </div>
         </div>
     </div>
