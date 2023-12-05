9acfb3fbe260b36d5c4018d2fe6c0850b77769a5
Marouane <marouane8loudini@gmail.com>
2023-12-05 13:30:48 +0100

    debut de club

--- controller/clubcontroller.php
+++ controller/clubcontroller.php
@@ -1,20 +1,179 @@
 <?php
-include('../model/bdcclub.php');
+include_once '../model/bdclub.php';
 
-$selectedYear = $_GET['quantity'] ?? '2023/2024';
-$podium = getPodiumByAnnee($selectedYear);
-$classement = getClassement($selectedYear);
+function getNomClubController($idEquipe) {
+    try {
+        $resultat = getNomClub($idEquipe);
 
-$premiereEquipe = getEquipeInfo($podium, 0);
-$deuxiemeEquipe = getEquipeInfo($podium, 1);
-$troisiemeEquipe = getEquipeInfo($podium, 2);
+        if (!empty($resultat)) {
+            $nomClub = $resultat[0]['nom'];
+        } else {
+            echo "Club non trouvé.";
+        }
+    } catch (PDOException $e) {
 
-$nomPremiereEquipe = $premiereEquipe['nom'];
-$imagePremiereEquipe = $premiereEquipe['photologo'];
+        echo "Erreur de base de données : " . $e->getMessage();
+    }
 
-$nomDeuxiemeEquipe = $deuxiemeEquipe['nom'];
-$imageDeuxiemeEquipe = $deuxiemeEquipe['photologo'];
+    return $nomClub;
+}
 
-$nomTroisiemeEquipe = $troisiemeEquipe['nom'];
-$imageTroisiemeEquipe = $troisiemeEquipe['photologo'];
+function getPhotoLigue($idEquipe) {
+    try {
+        $resultat = getLigue($idEquipe);
+
+        if (!empty($resultat)) {
+            $photoLigue = $resultat['photoLigue'];
+        } else {
+            echo "Aucune donnée trouvée.";
+        }
+    } catch (PDOException $e) {
+
+        echo "Erreur de base de données : " . $e->getMessage();
+    }
+
+    return $photoLigue;
+}
+
+function getLogoEquipe($idEquipe) {
+    try {
+        $resultat = getLogo($idEquipe);
+        if (!empty($resultat)) {
+            $photologo = $resultat['photologo'];
+        } else {
+            echo "Aucune donnée trouvée.";
+        }
+    } catch (PDOException $e) {
+        echo "Erreur de base de données : " . $e->getMessage();
+    }
+
+    return $photologo;
+}
+
+function getDescription($idEquipe) {
+    try {
+        $resultat = getDesc($idEquipe);
+        if (!empty($resultat)) {
+            $descr = $resultat['descr'];
+        } else {
+            echo "Aucune donnée trouvée.";
+        }
+    } catch (PDOException $e) {
+        echo "Erreur de base de données : " . $e->getMessage();
+    }
+
+    return $descr;
+}
+
+
+function getLogosanc($idEquipe) {
+    try {
+        $resultat = getLogosAncien($idEquipe);
+        $photos = array(); 
+
+        if (!empty($resultat)) {
+            foreach ($resultat as $row) {
+                $photos[] = $row['photologo']; 
+            }
+        } else {
+            echo "Aucune donnée trouvée.";
+        }
+    } catch (PDOException $e) {
+        echo "Erreur de base de données : " . $e->getMessage();
+    }
+
+    return $photos; 
+}
+
+
+function getPhotoStade($idEquipe) {
+    try {
+        $resultat = getImageStade($idEquipe);
+
+        if (!empty($resultat)) {
+            $PhotoStade = $resultat['photoStade'];
+        } else {
+            echo "Aucune donnée trouvée.";
+        }
+    } catch (PDOException $e) {
+
+        echo "Erreur de base de données : " . $e->getMessage();
+    }
+
+    return $PhotoStade;
+}
+
+function getDescrStade($idEquipe) {
+    try {
+        $resultat = getDescriptionStade($idEquipe);
+        if (!empty($resultat)) {
+            $descr = $resultat['descrStade'];
+        } else {
+            echo "Aucune donnée trouvée.";
+        }
+    } catch (PDOException $e) {
+        echo "Erreur de base de données : " . $e->getMessage();
+    }
+
+    return $descr;
+}
+
+
+
+function getPalmares($idEquipe) {
+    $Palamares = null;
+    try {
+        $resultat = getPhotoNbCoupe($idEquipe);
+        if (!empty($resultat) && isset($resultat['photoTrophee'])) {
+            $Palamares = $resultat['photoTrophee']['nombre'];
+        } else {
+            echo "Aucune donnée trouvée.";
+        }
+    } catch (PDOException $e) {
+        echo "Erreur de base de données : " . $e->getMessage();
+    }
+   
+    return $Palamares;
+   
+}
+
+
+function getPhotoCapi($idEquipe) {
+    try {
+        $resultat = getPhotoCapitaine($idEquipe);
+
+        if (!empty($resultat)) {
+            $photoCapitaine = $resultat['photoCapitaine'];
+        } else {
+            echo "Aucune donnée trouvée.";
+        }
+    } catch (PDOException $e) {
+
+        echo "Erreur de base de données : " . $e->getMessage();
+    }
+
+    return $photoCapitaine;
+}
+
+
+function getPhotoCoach($idEquipe) {
+    try {
+        $resultat = getPhotoEntraineur($idEquipe);
+
+        if (!empty($resultat)) {
+            $photoEntraineur = $resultat['photoEntraineur'];
+        } else {
+            echo "Aucune donnée trouvée.";
+        }
+    } catch (PDOException $e) {
+
+        echo "Erreur de base de données : " . $e->getMessage();
+    }
+
+    return $photoEntraineur;
+}
+
+
+
+
 
--- vue/vueclub.php
+++ vue/vueclub.php
@@ -1,3 +1,59 @@
 <?php
-    include('../controller/clubcontroller.php');
+include('../controller/clubcontroller.php');
+$idEquipe = isset($_GET['id']) ? intval($_GET['id']) : 0;
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title> <?php echo getNomClubController($idEquipe) ?></title>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&family=Open+Sans:wght@300;400;500&display=swap"
+    rel="stylesheet" />
+  <link rel="stylesheet" href="../css/styleclub.css" />
+</head>
+<body>
+<div class="accueil">
+    <div class="navbar">
+      <img class="logo" src="../ressources/club/logo.png" alt="Logo">
+      <nav>
+        <a class="acc" href="../index.php">Accueil</a>
+        <a href="#podium">Podium</a>
+        <a href="#Classement">Classement</a>
+        <a href="#Contact">Contact</a>
+      </nav>
+    </div>
+    <div class="landing">
+      <h1><?php echo getNomClubController($idEquipe); ?></h1>
+      <img src="../<?php echo getPhotoLigue($idEquipe) ?>" alt="Ligue de l'equipe" width="10%">
+      <img src="../<?php echo getLogoEquipe($idEquipe)?>" alt="Logo de l'equipe"width="20%">
+      <p><?php echo getDescription($idEquipe); ?></p>
+    </div>
+    <div class="logoancien">
+      <h1>Depuis 1900</h1>
+      <div class="leslogo">
+    <?php
+    $logos = getLogosanc($idEquipe);
+    foreach ($logos as $logo) {
+      echo '<img src="../' . $logo . '" alt="" width="10%">';
+    }
+    ?>
+  </div>
+    </div>
+</body>
+</html>
+
+
+<?php
+    //$logos = getLogosanc($idEquipe);
+       // foreach ($logos as $logo) {
+        //    echo $logo;?> <br><?php 
+       // }?><br><?php
+   // echo getPhotoStade($idEquipe);?><br><?php
+   // echo getDescrStade($idEquipe);?><br><?php
+   // echo getPhotoCoach($idEquipe);?><br><?php
 ?>
